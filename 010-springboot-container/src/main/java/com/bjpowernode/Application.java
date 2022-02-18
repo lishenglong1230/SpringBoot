148982249307cc46fa5d8058c91aaf8fbb510163
lsl <1215288388@qq.com>
2022-02-18 22:17:32 +0800

    springboot-container

---
+++ 010-springboot-container/src/main/java/com/bjpowernode/Application.java
@@ -0,0 +1,24 @@
+package com.bjpowernode;
+
+import com.bjpowernode.service.UserService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		//获取容器对象
+		//ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		//从容器中获取对象
+		UserService userService = (UserService)ctx.getBean("userService");
+		userService.sayHello("李四");
+
+	}
+
+}
