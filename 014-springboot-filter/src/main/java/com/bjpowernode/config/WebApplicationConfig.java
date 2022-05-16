d949b123b64f251b1e3665653c55fa0e5518db29
lsl <1215288388@qq.com>
2022-05-16 18:00:54 +0800

    springboot-filter

---
+++ 014-springboot-filter/src/main/java/com/bjpowernode/config/WebApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.config;
+
+import com.bjpowernode.web.MyFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebApplicationConfig {
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean bean  = new FilterRegistrationBean();
+        bean.setFilter( new MyFilter());
+        bean.addUrlPatterns("/user/*");
+        return bean;
+    }
+}
---
+++ 014-springboot-filter/src/main/java/com/bjpowernode/controller/CustomFilterController.java
@@ -0,0 +1,24 @@
+package com.bjpowernode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomFilterController {
+
+    @RequestMapping("/user/account")
+    @ResponseBody
+    public String userAccount(){
+        return "user/account";
+    }
+
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public String queryAccount(){
+        return "/query";
+    }
+
+
+}
